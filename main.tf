cf58bc3f9e56e92394b4b6a999205ad39be3808a
Iamneo <techadmin@iamneo.ai>
2023-09-21 15:18:17 +0000

    Backup Commit

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 provider "aws" {
     region = "ap-southeast-1" #Singapore region
+    access_key = "AKIAWSFYPBVFE6AZS5GE"
+    secret_key = "sPz4lcicbYFNYWRp0YlwkghxYgbXmZ01CQ1vFLkQ"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0839f419042b6d609"
+    ami = "ami-0dacd9d37849a9f38"
+}
+
+output "public_ip"{
+    value= aws_instance.example.public_ip
 }
